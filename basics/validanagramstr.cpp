67d1c8874c6d2c01e88d19408cea327dc6937235
sashpawar11 <saisaipawar@gmail.com>
2025-07-17 10:35:43 +0530

    anagramoptimiozd

--- basics/validanagramstr.cpp
+++ basics/validanagramstr.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 bool isAnagram(string s, string t) {
 
-     
+        //brute force
         if(s.length() != t.length()) return false;
 
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         
         return s==t;
+
+
+        //alternate method
+        // store frequencies of each char from s in a count arr
+        // iterate through t and decrease the freq for occurences in count arr
+        // iterate through final count arr, if all have values as 0 then it is an anagram
 	
 }
 int main() {
