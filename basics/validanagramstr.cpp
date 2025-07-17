72fbebad58dea652ad2401e2c894a5ba7f6c99c8
sashpawar11 <saisaipawar@gmail.com>
2025-07-17 10:31:00 +0530

    anagramstr

--- basics/validanagramstr.cpp
+++ basics/validanagramstr.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+bool isAnagram(string s, string t) {
+
+     
+        if(s.length() != t.length()) return false;
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        return s==t;
+	
+}
 int main() {
- 
- 
+    string s;
+     string t;
+
+    cin>> s;
+    cin >> t;
+    cout << isAnagram(s,t);
     return 0;
+ 
 }
